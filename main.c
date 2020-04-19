f8330489db5dbb02c47ed041cffac16b575dd51e
Toby Cathcart Burn <tcathcartburn@gmail.com>
2020-04-20 00:19:00 +0100

    Correct communication; Interface work

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #include<time.h>
 
 #define WIDTH (1<<10)
-#define HEIGHT (1<<10)
+#define HEIGHT (1<<9)
 
 #define MAXPLAYERS (1<<12)
 
@@ -52,7 +52,17 @@ char startResp[] = "HTTP/1.1 200 OK\r\n"
 "form.submit()"
 "</script></body></html>";
 
+char okResp[] = "HTTP/1.1 204 No Content\r\n"
+"Connection: close\r\n"
+//"Cache-Control: no-cache\r\n"
+"Content-Length: 0\r\n\r\n";
+
 char initChunk[10009];
+
+char hammerResp[23000]="HTTP/1.1 200 OK\r\n"
+"Content-Type: text/javascript\r\n"
+"Connection: close\r\n"
+"Content-Length:";
 
 // WIDTH=1024, 4*(1026/3)= 1368
 #define DATASIZE (2*HEIGHT)
@@ -74,17 +84,25 @@ char* startplnum;
 char* startp[2];
 char* startsz[2];
 
+char newplayerChunk[] = "XX\r\n"
+"<script>"
+"np(NNNNN ,XXXX ,YYYY )"
+"</script>\n"
+"\r\n";
+char* npdata;
 
 ///UTILS
 
 int snd(int sock, char* msg){
     printf("\nsending message to socket %d\n",sock);
-    puts(msg);
     int l = strlen(msg);
+    if (l<100) puts(msg);
+    
     while (l>0){
         int c = send(sock,msg,l,0);
         printf("sent: %d\n",c);
         if(c<0){perror("send failed"); close(sock); return -1;}
+        msg+=c;
         l-=c;
     }
     return 0;
@@ -92,6 +110,15 @@ int snd(int sock, char* msg){
 char tohexdig(int n){
   if(n<10) return 0x30+n;
   else return 0x61-10+n;
+}
+
+unsigned int readnat(char** s){
+    unsigned int n=0;
+    while(**s>='0' && **s<='9'){
+        n = n*10 + **s - '0';
+        (*s)++;
+    }
+    return n;
 }
 
 char b64[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -125,7 +152,7 @@ char* b64e(unsigned char* in, unsigned char* out, int len){
     return out;
 }
 
-int b64d(unsigned char* in, unsigned char* out, int len){
+unsigned char* b64d(unsigned char* in, unsigned char* out, int len){
     int lenin = (len/3)*4 + ((len%3)*3+1)/2;
     int tmp=0;
     int c=0;
@@ -141,12 +168,14 @@ int b64d(unsigned char* in, unsigned char* out, int len){
         else tmp<<=6;
     }
     int k = c%4;
-    tmp<<=(3-k)*6;
-    for(int i=0;i<k-1;i++){
-        *(out++)=(tmp>>16)&0xFF;
-        tmp<<=8;
+    if(k){
+        tmp<<=(3-k)*6;
+        for(int i=0;i<k-1;i++){
+            *(out++)=(tmp>>16)&0xFF;
+            tmp<<=8;
+        }
     }
-    return c;
+    return in;
 }
 
 void b64setup(){
@@ -181,7 +210,8 @@ typedef struct player player;
 player players[1<<16];
 
 int ixs[MAXPLAYERS];
-int plays[MAXPLAYERS+1];
+int plays[MAXPLAYERS];
+int numplays;
 int playing;
 
 int width; //for dynamic resizing
@@ -195,7 +225,7 @@ cell get(int x, int y){
 }
 
 void step(){
-    for(int x=0;x<width;x++) for(int y=0;y<width;y++){
+    for(int x=0;x<width;x++) for(int y=0;y<height;y++){
       cell v=grid[x][y];
       if(v){
           int numset=0;
@@ -235,16 +265,20 @@ void step(){
     void* tmp=grid;
     grid=next;
     next=tmp;
-    int i=0;
-    while(players[plays[i]].move[0]==-1){
-        short* x = players[plays[i]].move;
-        if(grid[x[0]][x[1]]==0){
-            //Add move to script
-            grid[x[0]][x[1]]=1;
-        }
-        players[plays[i]].move[0]=-1;
-    }
-    // Consider changing width/height
+}
+
+/// PLAYER HANDLING
+int remove_player(int idx){
+    printf("removing player %d\n",idx);
+    if(!players[idx].conn) return 1;
+    //close socket? - No becuase snd already does that
+    players[idx].conn=0;
+    int r = players[idx].ref;
+    ixs[r] = ixs[--playing];
+    players[ixs[r]].ref=r;
+    return 0;
+    // Consider decolouring cells belonging to the player
+    // Performance considerations apply if many disconnect simultaneously
 }
 
 
@@ -279,20 +313,86 @@ int sendStart(int idx, short* startpos ){
     b64e((char*) &players[idx].token, starttok, sizeof(long));
     return snd(players[idx].conn,startChunk);
 }
+void send_all(char* msg){
+    puts("sending");
+    puts(msg);
+    int l=strlen(msg);
+    while(l>0){
+        int i=0;
+        int sz=l<1024?l:1024;
+        while(i<playing){
+            int idx=ixs[i];
+            if (players[idx].ref!=i)
+                printf("prob: %d,%d,%d\n",i,idx,players[idx].ref);
+            int k=sz;
+            char* t=msg;
+            int c;
+            int sock = players[idx].conn;
+            if(sock==0){
+                printf("problem: %d\n",idx);
+                i+=1;continue;
+            }
+            while(k>0){
+                c = send(sock,t,k,0);
+                if(c<0)break;
+                k-=c;
+                t+=c;
+            }
+            if(c<0){
+                close(sock);
+                remove_player(idx);
+            }
+            else i++;
+        }
+        l-=sz;
+        msg+=sz;
+    }
+}
 
+char stepChunk[10*MAXPLAYERS+33]="XXXX\r\n"
+"<script>tick(";
+char* moveData;
+
+void startScript(){
+    moveData=stepChunk+19;
+}
+void addMove(unsigned short* x){
+    moveData+=sprintf(moveData,"%d,%d,",x[0],x[1]);
+}
+void endScript(){
+    moveData=strcpy(moveData,");</script>\n\r\n");
+    sprintf(stepChunk,"%04x",moveData-stepChunk+14-8);
+    stepChunk[4]='\r';
+}
+
+void sendNewPlayer(int idx, short* xy){
+    sprintf(npdata, "%5d ,%4d ,%4d", idx, xy[0], xy[1]);
+    npdata[5+2+4+2+4]=' ';
+    send_all(newplayerChunk);
+}
 
 /// REQUEST HANDLERS
 
-int remove_player(int idx){
-    if(!players[idx].conn) return 1;
-    //close socket? - No becuase snd already does that
-    players[idx].conn=0;
-    int r = players[idx].ref;
-    ixs[r] = ixs[--playing];
-    players[ixs[r]].ref=r;
-    return 0;
-    // Consider decolouring cells belonging to the player
-    // Performance considerations apply if many disconnect simultaneously
+void tick(){
+    step();
+    int i=0;
+    printf("%d\n",numplays);
+    startScript();
+    while(i<numplays){
+        unsigned short* x = players[plays[i]].move;
+        if(x[0]<width && x[1]<height && grid[x[0]][x[1]]==0){
+            //Add move to script
+            addMove(x);
+            grid[x[0]][x[1]]=1;
+        }
+        players[plays[i]].move[0]=-1;
+        i++;
+    }
+    numplays=0;
+    // Consider changing width/height
+    //Send script to everyone
+    endScript();
+    send_all(stepChunk);
 }
 
 int new_player(int sock){
@@ -308,7 +408,6 @@ int new_player(int sock){
     players[idx].conn=sock;
     players[idx].move[0]=-1;
     players[idx].ref=playing;
-    ixs[playing ++]=idx;
     
     // Setup start location
     short startpos[2];
@@ -318,8 +417,10 @@ int new_player(int sock){
     for(int i=width-1;i<width+3;i++)
       for(int j=height-1;j<height+3;j++){
         grid[(i+startpos[0])%width][(j+startpos[1])%height]=((i&2)||(j&2))?0:idx;
-        //printf("%d,%d:%d\n", (i+startpos[0])%width, (j+startpos[1])%height,grid[(i+startpos[0])%width][(j+startpos[1])%height]);
+        printf("%d,%d:%d\n", (i+startpos[0])%width, (j+startpos[1])%height,grid[(i+startpos[0])%width][(j+startpos[1])%height]);
     }
+    sendNewPlayer(idx,startpos);
+    ixs[playing ++]=idx;
     if(snd(sock,chunkedHeaders)
       || snd(sock,initChunk)
       || sendStart(idx,startpos)
@@ -327,6 +428,26 @@ int new_player(int sock){
     ){
         remove_player(idx);
     }
+    return 0;
+}
+
+int play_move(char* request){
+    unsigned int idx = readnat(&request);
+    if(*(request++)!='/') return -1;
+    if(idx>(1<<16) || players[idx].conn==0) return -1;
+    long t;
+//    printf("%d, %d\n",idx,players[idx].move[0]);
+    request=b64d(request,(unsigned char*) &t, sizeof(long));
+    if(t!=players[idx].token)return -1;
+    while(*request=='=')request++;
+    if(*(request++)!='/') return -1;
+    unsigned int x=readnat(&request);
+    if(x>=width || *(request++)!='/') return -1;
+    unsigned int y=readnat(&request);
+    if(y>=height) return -1;
+    if (players[idx].move[0]==-1)plays[numplays++]=idx;
+    players[idx].move[0]=x;
+    players[idx].move[1]=y;
     return 0;
 }
 
@@ -342,7 +463,18 @@ void handle(int sock, char* request){
         request+=4;
         if (memcmp(request,"/ ",2)==0)
             snd(sock,startResp);
-        else snd(sock,errResp);
+        else if(memcmp(request,"/hammer.min.js ",15)==0)
+            snd(sock,hammerResp);
+        else {
+            snd(sock,errResp);
+            /*CHECK(*(request++) =='/')
+            unsigned int x=readnat(&request);
+            CHECK(*(request++) =='/')
+            unsigned int y=readnat(&request);
+            printf("%d\n",grid[x][y]);
+            snd(sock,okResp);
+            */
+        }
     }
     else if(memcmp(request,"POST ",5)==0){
         request+=5;
@@ -352,7 +484,8 @@ void handle(int sock, char* request){
             return;// don't close socket
         }
         // Playing a move
-        snd(sock, errResp);
+        if (play_move(request)) snd(sock,errResp);
+        else snd(sock, okResp);
     }
     else CHECK(false)
     close(sock);
@@ -373,6 +506,7 @@ int mkChunk(char* str){
 int setup(){
     b64setup();
     if (mkChunk(startChunk)) return -1;
+    if (mkChunk(newplayerChunk)) return -1;
     starttok = strchr(startChunk,'T');
     startplnum = strchr(startChunk,'N');
     startp[0] = strchr(startChunk,'X');
@@ -380,6 +514,7 @@ int setup(){
     startsz[0] = strchr(startChunk,'W');
     startsz[1] = strchr(startChunk,'H');
     dataStart = strchr(dataChunk,'X');
+    npdata = strchr(newplayerChunk,'N');
     FILE * file = fopen("init.html","r");
     if (file==0){return -2;}
     int k = fread(initChunk+6,1,10000,file);
@@ -389,8 +524,20 @@ int setup(){
     initChunk[5]='\n';
     sprintf(initChunk+k+6,"\r\n");
     fclose(file);
+
+    file = fopen("hammer.min.js","r");
+    if (file==0){return -2;}
+    int l = strlen(hammerResp);
+    k = fread(hammerResp+l+9,1,22000,file);
+    if(ferror(file)) return -3;
+    if(!feof(file)) return -4;
+    int eight=sprintf(hammerResp+l,"%5d\r\n\r",k);
+    if(eight!=8) return -5;
+    hammerResp[l+eight]='\n';
+    sprintf(hammerResp+l+eight+k,"\r\n");
+    
     width=32;
-    height=32;
+    height=40;
     return 0;
 }
 
@@ -408,13 +555,14 @@ int main(){
         puts("Could not create socket");
         return 1;
     }
+    signal(SIGPIPE, SIG_IGN);
 
     //Really helps debugging
     int enable = 1;
     setsockopt(sock_listen, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
     struct timeval timeout;
-    timeout.tv_sec=1;
-    timeout.tv_usec=0;
+    timeout.tv_sec=0;
+    timeout.tv_usec=100000;
     if(setsockopt(sock_listen, SOL_SOCKET, SO_RCVTIMEO,
        &timeout, sizeof(timeout))<0){
         puts("setting timeout failed");
@@ -451,8 +599,8 @@ int main(){
         }
         time_t new = time(0);
         if(new!=prev){
-            step();
-            new=prev;
+            tick();
+            prev=new;
         }
     }
     return 0;
